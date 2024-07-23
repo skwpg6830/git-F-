223ca1fff70840e4c84115980b894f1f61e09cd2
skwpg6830 <skwpg6830@gmail.com>
2024-07-23 10:22:46 +0800

    購物車

--- 購物車/自己/font/vuetify-project/src/pages/products/[id].vue
+++ 購物車/自己/font/vuetify-project/src/pages/products/[id].vue
@@ -9,16 +9,27 @@
     <v-col cols="12">
       <p>${{ product.price }}</p>
       <p>{{ product.description }}</p>
+      <v-form :disabled="isSubmitting" @submit.prevent="submit">
+        <v-text-field type="number" v-model.number="quantity.value.value" :error-messages="quantity.errorMessage.value"></v-text-field>
+        <v-btn type="submit" prepend-icon="mdi-cart" :loading="isSubmitting">加入購物車</v-btn>
+      </v-form>
     </v-col>
   </v-container>
+  <v-overlay class="align-center justify-center text-center" :model-value="!product.sell" persistent>
+    <h1 class="text-center text-red">已下架</h1>
+    <v-btn to="/">回首頁</v-btn>
+  </v-overlay>
 </template>
 
 <script setup>
 import { ref } from 'vue'
 import { definePage } from 'vue-router/auto'
-import { useRoute } from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
 import { useApi } from '@/composables/axios'
 import { useSnackbar } from 'vuetify-use-dialog'
+import { useForm, useField } from 'vee-validate'
+import * as yup from 'yup'
+import { useUserStore } from '@/stores/user'
 
 definePage({
   meta: {
@@ -30,7 +41,9 @@ definePage({
 
 const { api } = useApi()
 const route = useRoute()
+const router = useRouter()
 const createSnackbar = useSnackbar()
+const user = useUserStore()
 
 const product = ref({
   _id: '',
@@ -65,4 +78,29 @@ const load = async () => {
   }
 }
 load()
+
+const schema = yup.object({
+  quantity: yup.number().typeError('數量必填').required('數量必填').min(1, '最少為 1')
+})
+const { isSubmitting, handleSubmit } = useForm({
+  validationSchema: schema,
+  initialValues: {
+    quantity: 1
+  }
+})
+const quantity = useField('quantity')
+
+const submit = handleSubmit(async (values) => {
+  if (!user.isLogin) {
+    router.push('/login')
+    return
+  }
+  const result = await user.addCart(product.value._id, values.quantity)
+  createSnackbar({
+    text: result.text,
+    snackbarProps: {
+      color: result.color
+    }
+  })
+})
 </script>
